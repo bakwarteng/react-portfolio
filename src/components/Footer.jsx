d27e53fd7c957a66d5175b4ee789dc7f2fabb93d
bakwarteng <bakwarteng@hotmail.com>
2024-07-12 17:59:59 -0500

    updated portfolio page with bootstrap cards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,49 +1,47 @@
 import React from "react";
 
 
+
+
   const footerStyle = {
+  
     position: "fixed",
     bottom: "0",
+    alignContent: "center",
     width: "100%",
-    
+
     backgroundColor: "rgb(250, 178, 172)",
     fontWeight: "bolder",
     textColor: "white",
     textAlign: "center",
-    padding: "10px",
+    padding: "1px",
   };
 
 function Footer() {
   return (
-   <div>
-    <footer style={footerStyle}>
- 
-      <a className="footerlinks"
-        href="https://github.com/bakwarteng"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
-      <br />
-      <a
-        href="https://linkedin.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
-      <br />
-      <a
-        href="https://stackoverflow.com/users/youruserid"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Stack Overflow
-      </a>
-    </footer>
+    <div>
+      
+
+      <footer style={footerStyle}>
+        <a
+          className="footerlinks"
+          href="https://github.com/bakwarteng"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <br />
+        <a
+          href="https://www.linkedin.com/in/beth-ann-kwarteng/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <br />
+      </footer>
     </div>
-    
   );
 
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,81 +1,88 @@
 import React from "react";
-import quiz from "../assets/images/quiz-screenshot.png"
-import projecttwo from "../assets/images/project-two-screenshot.png"
-import notetakerapp from "../assets/images/notetakerapp.png"
-import weatherdashboard from "../assets/images/weatherdashboard.png"
-import bebeblogs from "../assets/images/bebeblogs.png"
-import texteditor from "../assets/images/texteditor.png"
-
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import style from "../assets/styles/project.css"
+import projecttwo from "../assets/images/project-two-screenshot.png";
+import notetakerapp from "../assets/images/notetakerapp.png";
+import weatherdashboard from "../assets/images/weatherdashboard.png";
+import bebeblogs from "../assets/images/bebeblogs.png";
+import booksearchapi from "../assets/images/booksearchapi.png";
+import rentride from "../assets/images/rentride.png";
 
 function Portfolio() {
   const projects = [
     {
-      title: "QuizpicableMe",
-      image: quiz,
-      deployedLink: "https://sashuttle.github.io/QuizpicableMe/",
-      githubLink: "https://github.com/Sashuttle/QuizpicableMe",
-    },
-    {
-      title: "Happy Tails",
-      image: projecttwo,
-      deployedLink: "https://project-two-app-happy-tails.onrender.com",
-      githubLink: "https://github.com/bakwarteng/project-two-app "
+      title: "Rent and Ride",
+      description:
+        "Rent & Ride is a web application that enables users to rent cars for specific periods. It features car listings, reservation management, and user authentication for a seamless rental experience.",
+      image: rentride,
+      deployedLink: "https://rent-ride-jzpq.onrender.com/",
+      githubLink: "https://github.com/14jjfenlason/car-rental",
     },
 
     {
+      title: "Happy Tails",
+      description:
+        "Happy Tails is a website designed for animal lovers interested in adopting cats and dogs. Users can view available animals, access detailed information about each pet, and proceed with the adoption process through an interactive, user-friendly interface.",
+      image: projecttwo,
+      deployedLink: "https://project-two-app-happy-tails.onrender.com",
+      githubLink: "https://github.com/bakwarteng/project-two-app ",
+    },
+    {
       title: "Note Taker ",
+      description:
+        "Note Taker is a web application that allows users to create, save, and view notes. It features an intuitive interface where users can manage their notes efficiently, ensuring quick access and organization of their important information.",
       image: notetakerapp,
       deployedLink: " https://note-taking-app-0yuk.onrender.com",
       githubLink: "https://github.com/bakwarteng/note-taking-app",
     },
     {
       title: "Weather Dashboard",
+      description:
+        "Weather Dashboard is a web application that uses the Open Weather Map API to display the current weather and a 5-day forecast for searched cities. It features a user-friendly interface with real-time weather updates and forecast information.",
+
       image: weatherdashboard,
       deployedLink: "https://bakwarteng.github.io/weather-app/ ",
       githubLink: "https://github.com/bakwarteng/weather-app",
     },
-
     {
       title: "Bebe Blogs",
+      description: "Bebe Blogs is a simple blog page where users can enter their username, blog title, and content. The information is stored locally and displayed on the posts page, providing a straightforward blogging experience using HTML, CSS, and JavaScript.",
+
+
       image: bebeblogs,
       deployedLink: "https://bakwarteng.github.io/Bebe-blogs/",
       githubLink: "https://github.com/bakwarteng/Bebe-blogs",
     },
-
     {
-      title: "Text Editor",
-      image: texteditor,
-      deployedLink: "https://bakwarteng.github.io/Bebe-blogs/",
-      githubLink: "https://github.com/bakwarteng/Bebe-blogs",
-    }
+      title: "Book Search API",
+      description:
+        "Book Search API is a Node.js application that enables users to search for books, view details, and manage their favorite books. It features a secure login system and utilizes Mongoose, GraphQL, and Apollo Server for robust data handling and API management.",
+      image: booksearchapi,
+      deployedLink: "https://book-search-api-0dc1.onrender.com",
+      githubLink: "https://github.com/bakwarteng/book-search-api ",
+    },
   ];
 
   return (
-    <section>
-      <h2  className="portheader">Portfolio</h2>
-      <div className="col-12 text-center">
-        {projects.map((project, index) => (
-          <div key={index}  className="portfolio-item">
-            <img  src={project.image} alt={project.title} className="portimages" />
-            <h3>{project.title}</h3>
-            <a
-              href={project.deployedLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Live
-              <br></br>
-            </a>
-            <a
-              href={project.githubLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </div>
-        ))}
-      </div>
+    <section className="portcontent">
+      {projects.map((project, index) => (
+        <Card className="portcards" style={{ width: "20rem" }} key={index}>
+          <Card.Img variant="top" src={project.image} />
+          <Card.Body>
+            <Card.Title>{project.title}</Card.Title>
+            <Card.Text>{project.description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>
+              <a href={project.deployedLink}>Deployed Link</a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <a href={project.githubLink}>GitHub Link</a>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      ))}
     </section>
   );
 }
